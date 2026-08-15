d3ebebe48fcb413b09bcce14de0735ebda1e21f2
Soumyajit Rana <soumyajitrana280@gmail.com>
2026-08-15 23:56:00 +0530

    Add home route

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.signin, name="home"),
     path("signup/", views.signup, name="signup"),
     path("signin/",views.signin, name = "signin")
     ]
